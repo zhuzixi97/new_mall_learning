4b8c56e3fc8a35774841e964d82fcd68de52f0a5
zhu <qw12757@163.com>
2020-12-24 23:51:10 +0800

    架子

---
+++ src/main/java/com/zhu/mall_learning/MallLearningApplication.java
@@ -0,0 +1,13 @@
+package com.zhu.mall_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhu/mall_learning/MallLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhu.mall_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
